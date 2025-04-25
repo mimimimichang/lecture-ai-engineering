308deab47aecbed30c22de7bdb579f3c243a294d
mimimimichang <yamada33yamada@gmail.com>
2025-04-25 05:51:31 +0000

    modify: Switch to a different model again for insufficient disk space

--- day1/02_streamlit_app/config.py
+++ day1/02_streamlit_app/config.py
@@ -1,4 +1,5 @@
 # config.py
 DB_FILE = "chat_feedback.db"
 # MODEL_NAME = "google/gemma-2-2b-jpn-it"
-MODEL_NAME = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+# MODEL_NAME = "mistralai/Mixtral-8x7B-Instruct-v0.1" # Insufficient disk space
+MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
